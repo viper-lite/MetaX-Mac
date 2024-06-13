f8a352506ec25d8b7d56753da3a699d15ba89030
mylhyz <mylhyz@gmail.com>
2024-06-13 14:31:36 +0800

    添加MetaX原始项目

---
+++ MetaX/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetaX
+//
+//  Created by mylhyz on 2024/6/13.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
